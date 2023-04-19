dece6a0c1647441c1619430a07dd494d1aa298ac
Kamran <bytalibli2@gmail.com>
2023-04-19 15:13:45 +0400

    create-Db, Db-Querying

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    require_once 'core/init.php';
+
+   $user = DB::getInstance()->get('users', array('username', '=', 'alex'));
+   
+   if(!$user->count()) {
+    echo 'No user';
+   } else {
+    echo 'OK!';
+   }     
+?>
