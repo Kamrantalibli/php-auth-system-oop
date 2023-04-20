15eb249bd840ec517233040e045a501b179db3ba
Kamran <bytalibli2@gmail.com>
2023-04-20 11:09:13 +0400

    db insert/update

--- classes/DB.php
+++ classes/DB.php
@@ -70,6 +70,50 @@
             return $this->action('DELETE', $table, $where);
         }
 
+        public function insert($table, $fields = array()) {
+            $keys = array_keys($fields);
+            $values = null;
+            $x = 1;
+
+            foreach($fields as $field) {
+                $values .='?';
+                if($x < count($fields)) {
+                    $values .= ', ';
+                }
+                $x++;
+            }
+
+            $sql = "INSERT INTO users (`" . implode('` , `', $keys) . "`) VALUES ({$values})";
+
+            if(!$this->query($sql, $fields)->error()) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public function update($table, $id, $fields) {
+            $set = '';
+            $x = 1;
+
+            foreach($fields as $name => $value) {
+                $set .= "{$name} = ?";
+                if($x < count($fields)) {
+                    $set .= ', ';
+                }
+                $x++;
+            }
+
+            // die($set);
+            
+            $sql = "UPDATE {$table} SET {$set} WHERE id = {$id}";
+
+            if(!$this->query($sql, $fields)->error()) {
+                return true;
+            }
+            return false;
+        }
+
         public function results() {
             return $this->_results;
         }
--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
     require_once 'core/init.php';
 
-   $user = DB::getInstance()->get('users', array('username', '=', 'alex'));
+//    $user = DB::getInstance()->get('users', array('username', '=', 'alex'));
    
-   if(!$user->count()) {
-    echo 'No user';
-   } else {
-    echo $user->first()->username;
-   }     
+//    if(!$user->count()) {
+//     echo 'No user';
+//    } else {
+//     echo $user->first()->username;
+//    }    
+
+
+$user = DB::getInstance()->update('users', 6, array(
+    'password' => 'newPassword',
+    'name' => 'Alex Garrett'
+))
 ?>
